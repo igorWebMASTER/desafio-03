76bf25baf9b40d898172f10d84bcfa52b2f0807c
Igor Ribeiro <igor_1917@hotmail.com>
2020-02-20 21:35:47 -0300

    Add files via upload

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3cc2eef5e751ec',
+    pass: 'a1b7f33704ce07',
+  },
+  default: {
+    from: 'Equipe Fasfeet <noreply@fasfeet.com>',
+  },
+};
